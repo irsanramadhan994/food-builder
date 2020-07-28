da5076051f1016178fe9ff4686a02d90fa4e9239
Irsan Ramadhan <irsan.ramadhan994@gmail.com>
2020-07-28 11:05:56 +0700

    compnent

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+import Aux from '../../hoc/Auxiliary'
+
+class BurgerBuilder extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <Aux>
+                <div>
+                    Burger
+                </div>
+
+                <div>
+                    Burger Controls
+                </div>
+            </Aux>
+            
+        );
+    }
+}
+
+export default BurgerBuilder;
